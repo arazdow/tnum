8d77d82dac8600e7926caac848915f5e0d9df2a0
Allen Razdow <arazdow@truenum.com>
2020-07-01 00:08:26 -0400

    reading tweets with twitteR package

---
+++ tnum/R/twitterutilities.R
@@ -0,0 +1,8 @@
+# twitter access for testing
+library(twitteR)
+
+twitter_token <- setup_twitter_oauth(
+  consumer_key = "EJJSOPMbniEdgyxhD9Q6rZDp1",
+  consumer_secret = "tcMRH9XTmXd6nq9hAFsYtsHW5cwsymN32duLCNmQIoqb3amwja",
+  access_token = "1274782526926700546-PJpruW5N5CTkzycTj9gsZSePBdHv97",
+  access_secret = "pfUowEnIFZRWOzJ8DDETiCNtIkT0PheEBuJnOE4O6bZvl")
