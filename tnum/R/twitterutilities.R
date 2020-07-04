5aedac679e794d08df550013d7aee4fcd59a02db
Allen Razdow <arazdow@truenum.com>
2020-07-04 01:31:03 -0400

    progress with generation tn's from tweets

--- tnum/R/twitterutilities.R
+++ tnum/R/twitterutilities.R
@@ -7,29 +7,79 @@ library(twitteR)
 #' Authenticate twitteR package with Twitter account
 #'
 
-tnum.twt.authorize <- function(){
-twitter_token <- setup_twitter_oauth(
-  consumer_key = "EJJSOPMbniEdgyxhD9Q6rZDp1",
-  consumer_secret = "tcMRH9XTmXd6nq9hAFsYtsHW5cwsymN32duLCNmQIoqb3amwja",
-  access_token = "1274782526926700546-PJpruW5N5CTkzycTj9gsZSePBdHv97",
-  access_secret = "pfUowEnIFZRWOzJ8DDETiCNtIkT0PheEBuJnOE4O6bZvl")
+tnum.twt.authorize <- function() {
+  twitter_token <- setup_twitter_oauth(
+    consumer_key = "EJJSOPMbniEdgyxhD9Q6rZDp1",
+    consumer_secret = "tcMRH9XTmXd6nq9hAFsYtsHW5cwsymN32duLCNmQIoqb3amwja",
+    access_token = "1274782526926700546-PJpruW5N5CTkzycTj9gsZSePBdHv97",
+    access_secret = "pfUowEnIFZRWOzJ8DDETiCNtIkT0PheEBuJnOE4O6bZvl"
+  )
 }
 
-#' Create truenumber data frame from tweets dataframe
+#' @title Post new tnums from twitteR query result
 #'
-#' @param tnumdf dataframe of tweets returned from twitteR package
+#' tnums for each tweet:
+#'  1. tnum for full text
+#'  2. tnum for create date
+#'  3. tnum for favorite count (not present if 0)
+#'  4. tnum for retweet count (not present if 0)
+#'  5  tnum for replied-to tweet subject (not present if not a reply)
+#'  6. tnum for user location
 #'
-#' @return dataframe of truenumbers
+#' tags for each tweet:
+#'  1. tagged if retweet
+#'  2. tagged if truncated
+#'  3. tagged with user device
+#'
+#' @param tweetList List of tweets as returned from twitteR::Search()
+#'
+#' @return return code of tnum.maketruenumbers call
 #' @export N/A
 #'
-#'
-#' @examples
-tnum.twt.tweeteR2tnums <- function(tweetList){
- tf <- twListToDF(tweetList)
- subj.vector <- paste0("twitter/user:",tf$screenName,"/tweet:",tf$id)
- users <- unique(tf$screenName)
- profiles <- lookupUsers(users,TRUE)
- returnValue(profiles)
+
+tnum.twitteR.post_enriched_tnums <- function(tweetList) {
+  tf <- twitteR::twListToDF(tweetList)  #create data.frame from list
+
+  # make vectors for tnum parts
+  tweet.subj.vector <-
+    paste0("twitter/user:", tf$screenName, "/tweet:", tf$id)
+  tweet.prop.vector <- rep("text", length(tweet.subj.vector))
+  tweet.cvalue.vector <- paste0('"', lapply(tf$text, escapequotes), '"')
+
+  tweet.tags.platforms <-
+    paste0("tweet/platform:",
+           lapply(tf$statusSource, getTweetPlatform))
+  tweet.tags.truncated <-
+    paste0("tweet:truncated", lapply(tf$truncated, tagtruncated))
+
+  users <- unique(tf$screenName)
+  profiles <- lookupUsers(users, TRUE)
+  returnValue(profiles)
+
+  getTweetPlatform <- function(atag) {
+    if (str_contains(atag, "ipad", ignore.case = TRUE)) {
+      returnValue("iPad")
+    } else if (str_contains(atag, "android", ignore.case = TRUE)) {
+      returnValue("Android")
+    } else if (str_contains(atag, "web", ignore.case = TRUE)) {
+      returnValue("Web")
+    } else if (str_contains(atag, "iphone", ignore.case = TRUE)) {
+      returnValue("iPhone")
+    } else if (str_contains(atag, "linkedin", ignore.case = TRUE)) {
+      returnValue("LinkedIn")
+    }
+  }
+
+  escapequotes <- function(strng) {
+    returnValue(gsub( '"', '\\"',strng))
+  }
+
+  tagtruncated <- function(istrunc) {
+    if (istrunc) {
+      returnValue("tweet:truncated")
+    } else {
+      returnValue(NA)
+    }
+  }
+
 }
-
-
