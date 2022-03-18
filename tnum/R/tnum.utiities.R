fe1d01f2568d1cb975b6b7919d0930326a4fe3cd
Allen Razdow <arazdow@truenum.com>
2022-03-18 00:34:39 -0400

    df ingest dealing with long column names

--- tnum/R/tnum.utiities.R
+++ tnum/R/tnum.utiities.R
@@ -2,6 +2,7 @@
 
 tokenize <- function(aString){
   tok <- str_replace_all(str_trim(aString, side= "both"),"\\s+","_")
+  tok <- str_replace_all(tok,"[^a-zA-Z0-9'_]","-")
 }
 
 ########################################################
@@ -27,15 +28,18 @@ tnum.loadLibs <- function(){
 #' @param tag path used to tag all the created truenumbers
 #' @export
 
-tnum.ingestDataFrame <- function(df, subjectRoot, idColumn, tag = "origin:R/tnum"){
+tnum.ingestDataFrame <- function(df, subjectRoot, idType = "/row:", idColumn, tag = "origin:R/tnum"){
 
   dfRows <- dim(df)[[1]]
   dfCols <- dim(df)[[2]]
+  tnCount <- 0
+  longProps <- list()
 
   for(i in 1:3){
     for(j in 1:dfCols){
-      subj <- paste0(subjectRoot,tokenize(df[i,][[tokenize(idColumn)]]))
-      prop <- tokenize(names(df[i,][j]))
+      subj <- paste0(subjectRoot,idType, tokenize(df[i,][[tokenize(idColumn)]]))
+      colName <- names(df[i,][j])
+      prop <- tokenize(colName)
       val <- df[i,][[j]]
 
       #if value is mode character, quote it as a string
@@ -43,12 +47,36 @@ tnum.ingestDataFrame <- function(df, subjectRoot, idColumn, tag = "origin:R/tnum
         val <- str_trim(val, side = "both")
         val <- paste0("\"",val,"\"")
       }
-      if(!is.null(val) && (nchar(val) != 0) && !is.na(val)){
+      if(!is.na(val) && !is.null(val) && (nchar(val) > 2)){
+        ## deal with overlong column names
+        if(nchar(colName) > 25){
+
+          if(!(colName %in% longProps)){
+
+            longProps <- c(longProps,list(colName))
+
+            prop <- tokenize(substring(colName,1,25))
+            prop <- paste0(prop,":t",which(longProps == colName))
+
+            colStmt <- tnum.buildStatement(paste0(subjectRoot,"/column:",prop),
+                                           "text:heading:full",paste0("\"",colName,"\""))
+            print(paste0("  ---> ",colStmt))
+            res <- tnum.postStatement(colStmt,"had to truncate this heading",list(tag))
+          } else {
+            prop <- tokenize(substring(colName,1,25))
+            prop <- paste0(prop,":t",which(longProps == colName))
+          }
+        }
+
         stmt <- tnum.buildStatement(subj,prop,val)
         print(stmt)
         tnResult <- tnum.postStatement(stmt,"",list(tag))
+        tnCount <- tnCount + 1
+
       }
     }
+    print(paste0(tnCount, " TNs posted"))
+    return (tnCount)
   }
 
 
