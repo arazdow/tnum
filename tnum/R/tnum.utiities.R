49c317318f8546c60739328033c09d0f3167cbb7
Allen Razdow <arazdow@truenum.com>
2022-03-23 00:15:56 -0400

    enhanced ingester and more utilities

--- tnum/R/tnum.utiities.R
+++ tnum/R/tnum.utiities.R
@@ -24,12 +24,27 @@ tnum.loadLibs <- function(){
 #'
 #' @param df  the data frame, as returned by read.csv() for example
 #' @param subjectRoot string used as root of subject path. It should not end with : or /
-#' @param idType string appended to subjectRoot to prefix the idColumn token. Must start and end with : or /
-#' @param idColumn name of a column to be used at the end of the subject path to identify the row
+#' @param subjectTerms a list of string pairs appended to subjectRoot build the subject. The
+#' second string in each pair must be the name of a column in the df, and the first is a prefix
+#' placed in the path before the value of that column.
+#' @param propTerms similar to subjectTerms, but has pairs named by a df column name:
+#'   list(colname = c("path:property", "unit of measure")).  The property path if not empty will be
+#'   used as the TN property, and if empty, the column name will be used. The units are appended to the value.
 #' @param tag path used to tag all the created truenumbers
+#' @concept  ingestDataFrame(df, subjectRoot = "fauna",
+#'                          subjectTerms = list(c(":american/","type"), c(":", "status")),
+#'                          propTerms = list(c("density:population","pop-dens","items/acre")),
+#'                          tag = "demo:ingest")
+#'
+#'   This will produce a TN for each cell in the row of the form:
+#'
+#'   fauna:american/bison:endangered has density:population = 2.45 items/acre which expands to:
+#'
+#'   population density of endangered bison, american fauna is 2.45 items/acre
+#'
 #' @export
 
-tnum.ingestDataFrame <- function(df, subjectRoot, idType = "/row:", idColumn, tag = "origin:R/tnum"){
+tnum.ingestDataFrame <- function(df, subjectRoot, subjectTerms = list(), propTerms = list(),tag = "origin:R/tnum"){
 
   dfRows <- dim(df)[[1]]
   dfCols <- dim(df)[[2]]
@@ -37,10 +52,18 @@ tnum.ingestDataFrame <- function(df, subjectRoot, idType = "/row:", idColumn, ta
   longProps <- list()
 
   for(i in 1:dfRows){
+    subj <- subjectRoot
+    for(pair in subjectTerms){
+      subj = paste0(subj,pair[[1]], tokenize(df[i,][[pair[[2]]]]))
+    }
+    if(subj == subjectRoot){subj = paste0(subj,"/row:",i)}
     for(j in 1:dfCols){
-      subj <- paste0(subjectRoot,idType, tokenize(df[i,][[tokenize(idColumn)]]))
       colName <- names(df[i,][j])
       prop <- tokenize(colName)
+      if(!is.null(propTerms[[colName]])){
+        prop <- propTerms[[colName]]
+      }
+
       val <- df[i,][[j]]
 
       #if value is mode character, quote it as a string
@@ -51,14 +74,14 @@ tnum.ingestDataFrame <- function(df, subjectRoot, idType = "/row:", idColumn, ta
       }
       if(!is.na(val) && !is.null(val) && (nchar(val) > 2)){
         ## deal with overlong column names
-        if(nchar(colName) > 25){
+        if(nchar(prop) > 25){
 
-          if(!(colName %in% longProps)){
+          if(!(prop %in% longProps)){
 
-            longProps <- c(longProps,list(colName))
+            longProps <- c(longProps,list(prop))
 
-            prop <- tokenize(substring(colName,1,25))
-            prop <- paste0(prop,":t",which(longProps == colName))
+            props <- tokenize(substring(colName,1,25))
+            prop <- paste0(props,":t",which(longProps == colName))
 
             colStmt <- tnum.buildStatement(paste0(subjectRoot,"/column:",prop),
                                            "text:heading:full",paste0("\"",colName,"\""))
@@ -100,7 +123,7 @@ tnum.pathLength <- function(path){
 #'
 #' @param path the path
 #' @param n length of desired sub-path
-#' @return first n segments of path.
+#' @return first n terms of path.
 #' @export
 #'
 
